cd7a17714ac12f7808c59d7c550c42f1769fbdb1
concarne000 <49512595+concarne000@users.noreply.github.com>
2024-01-23 13:14:30 +1100

    Fixed hash lookup on caching, added multiple image files

--- __init__.py
+++ __init__.py
@@ -8,12 +8,22 @@ import shlex
 import os, urllib.request, re, threading, posixpath, urllib.parse, argparse, socket, time, hashlib, pickle, signal, imghdr, io
 import numpy as np
 import torch
+import hashlib
 
 from PIL import Image, ImageOps
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def pil2tensor(image):
-    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0)
+    
+def center_crop(img, new_width=None, new_height=None):        
+
+    widthcenter = img.width / 2
+    heightcenter = img.height / 2
+
+    center_cropped_img = img.crop((widthcenter - (new_width/2), heightcenter - (new_height/2), widthcenter + (new_width/2), heightcenter + (new_height/2)))
+
+    return center_cropped_img    
     
 class BingImageGrabber:
 
@@ -25,6 +35,7 @@ class BingImageGrabber:
         return {
             "required": {
                 "search_term": ("STRING", {"default": "dog"}),
+                "num_of_images": ("INT", {"default": 1}),
                 "cache_search": ("BOOLEAN", {"default": True},),
                 "cache_images": ("BOOLEAN", {"default": True},),
              },
@@ -43,7 +54,7 @@ class BingImageGrabber:
     def IS_CHANGED(cls, **kwargs):
         return float("NaN")
         
-    def test(self, search_term, cache_search, cache_images):
+    def test(self, search_term, num_of_images, cache_search, cache_images):
 
         iterations = 1
 
@@ -53,7 +64,7 @@ class BingImageGrabber:
         
         bing_txt.translate(dict.fromkeys(map(ord, u"/\&:+:%")))
         
-        texthash = str(hash(bing_txt))
+        texthash = str(hashlib.md5(bing_txt.encode()).hexdigest())
 
         urlopenheader={ 'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
         foundimage=None
@@ -61,8 +72,14 @@ class BingImageGrabber:
         if not os.path.exists("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"):
             os.makedirs("./ComfyUI/custom_nodes/ConCarneNode/searchcache/")
         
+        if (cache_search):
+            print ("Cache search enabled")
+        
+        if (os.path.isfile("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+".searchcache")):
+            print ("Cache found")
+        
         if (os.path.isfile("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+".searchcache")) and cache_search:
-            #print ("ok")
+            print ("Loading links from cache")
             text_file = open("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+".searchcache", "r")
             totallinks = text_file.readlines()
             text_file.close()
@@ -106,56 +123,81 @@ class BingImageGrabber:
         
         foundimage = None
         
-        if not os.path.exists("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+bing_txt) and cache_images:
-            os.makedirs("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+bing_txt)          
-                
-        for tries in range(10):                           
+        if not os.path.exists("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash) and cache_images:
+            os.makedirs("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash)          
         
-            url = totallinks[random.randint(0, len(totallinks) - 1)]
+        random.shuffle(totallinks)
+        
+        for index in range(len(totallinks)):
             
-            filename = posixpath.basename(url).split('?')[0] #Strip GET parameters from filename
-            name, ext = os.path.splitext(filename)
-            name = name[:36].strip()
-            name = name + str(hash(name))[0:4]
-            filename = (name + ext).strip()
-            
-            if os.path.isfile("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+bing_txt+"/"+filename) and cache_images:
-                foundimage = Image.open("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+bing_txt+"/"+filename)
-                #print ('Loading image from cache')
+            if (len(images) >= num_of_images):
                 break
             
-            #print ("downloading " + url)
+            for tries in range(10):                           
             
-            try:
-                request=urllib.request.Request(url,None,urlopenheader)
-                image_data=urllib.request.urlopen(request).read()
-            except:
-                continue
-                        
-            if not imghdr.what(None, image_data):
-                print('Invalid image, not loading')
-                continue
-            
-            foundimage = Image.open( io.BytesIO(image_data))
-            
-            if cache_images:
-                imagefile=open(os.path.join("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+bing_txt+"/", filename),'wb')
-                imagefile.write(image_data)
-                imagefile.close()
-            
-            break          
-           
+                url = totallinks[index]
+                
+                filename = posixpath.basename(url).split('?')[0] #Strip GET parameters from filename
+                name, ext = os.path.splitext(filename)
+                name = name[:36].strip()
+                name = name + str(hash(name))[0:4]
+                filename = (name + ext).strip()
+                
+                filenamehash = hashlib.md5( filename.encode() ).hexdigest()
+                
+                if os.path.isfile("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+"/"+filenamehash+".jpg") and cache_images:
+                    foundimage = Image.open("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+"/"+filenamehash+".jpg")
+                    print ('Loading image from cache')
+                    
+                else:
+                
+                    #print ("downloading " + url)
+                    
+                    try:
+                        request=urllib.request.Request(url,None,urlopenheader)
+                        image_data=urllib.request.urlopen(request).read()
+                    except:
+                        continue
+                                
+                    if not imghdr.what(None, image_data):
+                        print('Invalid image, not loading')
+                        continue
+                    
+                    foundimage = Image.open( io.BytesIO(image_data))
+                    
+                    if cache_images:
+                        #imagefile=open(os.path.join("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+"/", filenamehash+".jpg"),'wb')
+                        #imagefile.write(image_data)
+                        #imagefile.close()
+                        foundimage.save(os.path.join("./ComfyUI/custom_nodes/ConCarneNode/searchcache/"+texthash+"/", filenamehash+".jpg"))
 
-        image = foundimage
+                image = foundimage
+                
+                minsize = min(foundimage.width, foundimage.height)
+                
+                if (num_of_images > 1):
+                
+                    foundimage = center_crop(foundimage, minsize, minsize)
+                
+                    image = foundimage.resize((512,512))
+                    
+                else:
+                
+                    image = foundimage
+                    
+                image = ImageOps.exif_transpose(image)
+                image = image.convert("RGB")
+                image = pil2tensor(image).unsqueeze(0)
+                
+                images.append(image)
+                
+                break          
+ 
         
-        #image = foundimage.resize((512,512))
-        image = ImageOps.exif_transpose(image)
-        image = image.convert("RGB")
-        image = pil2tensor(image).unsqueeze(0)
         
         #print (image.size())
         
-        return (image)
+        return ( torch.cat(images, dim=0), )
 
 class Zephyr:
     
